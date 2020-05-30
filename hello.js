e45d9240d5e1012b085998a9c29a44954ac48e2a
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2020-05-30 20:27:54 +0300

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,24 +1,21 @@
-//Вывод полной даты и времени в консоль в русскоязычном варианте со склонениями
-let now = new Date(); //количество миллисекунд с 1 января 1970 года (в местной временной зоне)
-let day = now.getDate(); //возвращает день месяца
-let month = now.getMonth(); //возвращает номер месяца (январь = 0)
-let year = now.getFullYear(); //возвращает год (4 цифры)
-let today = now.getDay(); //возвращает номер дня недели, начиная с воскресенья
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let seconds = now.getSeconds();
-let currentMonth = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
-let dayOfWeek = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'];
+//Вывод полной даты и времени в консоль в русскоязычном варианте со склонениями, с интервалом 1 сек.
+setInterval(function () {
+  let d = new Date();
+  let day = d.getDate();
+  let month = d.getMonth();
+  let year = d.getFullYear();
+  let today = d.getDay();
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  let seconds = d.getSeconds();
+  let currentMonth = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+  let dayOfWeek = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
 
 // функция склонения чисел (n = что склоняем, titles = как склоняем)
-function declOfNum(n, titles) {
-  cases = [2, 0, 1, 1, 1, 2];
-  return titles[(n%100 > 4 && n%100 < 20) ? 2 : cases[(n%10 < 5) ? n%10 : 5]];
-}
+  function declOfNum(n, titles) {
+    cases = [2, 0, 1, 1, 1, 2];
+    return titles[(n % 100 > 4 && n % 100 < 20) ? 2 : cases[(n % 10 < 5) ? n % 10 : 5]];
+  }
 
-// //Второй вариант
-// function declOfNum(n, titles) {
-//   return titles[n%10 == 1 && n%100 != 11 ? 0 : n%10 >= 2 && n%10 <= 4 && (n%100 < 10 || n%100 >= 20) ? 1 : 2];
-// }
-
-console.log('Сегодня ' + day + ' ' + currentMonth[month] + ' ' + year + ' года, ' + dayOfWeek[today] + ', ' + hours + ' ' + (declOfNum(hours, ['час', 'часа', 'часов'])) + ' ' + minutes + ' ' + (declOfNum(minutes, ['минута', 'минуты', 'минут'])) + ' ' + seconds + ' ' + (declOfNum(seconds, ['секунда', 'секунды', 'секунд'])));
+  console.log('Сегодня ' + day + ' ' + currentMonth[month] + ' ' + year + ' года, ' + dayOfWeek[today] + ', ' + hours + ' ' + (declOfNum(hours, ['час', 'часа', 'часов'])) + ' ' + minutes + ' ' + (declOfNum(minutes, ['минута', 'минуты', 'минут'])) + ' ' + seconds + ' ' + (declOfNum(seconds, ['секунда', 'секунды', 'секунд'])));
+}, 1000);
